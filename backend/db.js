e0a5adac7817dbff87a225c8d5104cbc246c67b0
quanhizako <quanhizako.ptit@gmail.com>
2026-06-21 08:14:47 +0700

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -3,9 +3,9 @@ const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST || "localhost",
-  port: process.env.DB_PORT || 3307,
+  port: process.env.DB_PORT || 3306,
   user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "2007",
+  password: process.env.DB_PASSWORD || "",
   database: process.env.DB_NAME || "am_thuc_vn",
   waitForConnections: true,
   connectionLimit: 10,
